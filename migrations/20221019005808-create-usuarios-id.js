ed6e5320a68048f46e44c532e33b7e32ab813790
lucashb <lucas.henrique@matera.com>
2022-10-18 22:01:23 -0300

    primeiro commit

---
+++ migrations/20221019005808-create-usuarios-id.js
@@ -0,0 +1,48 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('usuarios', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      nome_usuario:{
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      moedas:{
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      valor:{
+        type: Sequelize.FLOAT,
+        allowNull: false
+      },
+      vidas:{
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      createdat:{
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedat:{
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
